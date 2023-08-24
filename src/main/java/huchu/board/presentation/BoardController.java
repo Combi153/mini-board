b2a00d283d734f4af5461324354dca72d5f04065
Combi153 <jcm04@naver.com>
2023-08-24 16:34:29 +0900

    [build] add spring-boot-starter-web dependency

--- src/main/java/huchu/board/presentation/BoardController.java
+++ src/main/java/huchu/board/presentation/BoardController.java
@@ -1,7 +1,7 @@
 package huchu.board.presentation;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class BoardController {
 }
